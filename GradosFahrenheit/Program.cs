f8c8f3dae344616566e28010572e8e39cceaa824
Romina <rportillo9419@gmail.com>
2026-04-27 18:44:13 -0300

    Entrega ejercicio Grados Fahrenheit

---
+++ GradosFahrenheit/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace GradosFahrenheit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingresa la temperatura en grados Celsius:");
+            
+            // El signo '?' soluciona el error CS8600
+            string? entrada = Console.ReadLine();
+
+            if (!string.IsNullOrEmpty(entrada))
+            {
+                double celsius = Convert.ToDouble(entrada);
+                double fahrenheit = (celsius * 9 / 5) + 32;
+
+                Console.WriteLine($"{celsius} grados Celsius equivalen a {fahrenheit} grados Fahrenheit.");
+            }
+        }
+    }
+}
